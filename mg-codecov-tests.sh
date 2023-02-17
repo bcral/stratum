cac097eb99c6cfae7782091b9a4ad631b7c1e062
dcoleman <dcoleman@foundrydigital.com>
2023-02-17 12:00:49 -0500

    fixed parsing problem and added backtrace

--- mg-codecov-tests.sh
+++ mg-codecov-tests.sh
@@ -2,4 +2,4 @@ message_generator_dir="./utils/message-generator/"
 
 cd $message_generator_dir
 
-cargo run -- ../../../test/message-generator/test/cov/cov_test.json
+RUST_BACKTRACE=1 cargo run -- ../../../test/message-generator/test/cov/cov_test.json
