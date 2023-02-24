b4a35ea3313709a33c40c6f8fef554a4cfa0dfb0
bcral <>
2023-02-24 13:06:33 -0500

    Testing code coverage for all MG tests at high level

--- message-generator-tests.sh
+++ message-generator-tests.sh
@@ -5,5 +5,7 @@ cd $message_generator_dir
 
 for entry in `ls $search_dir`; do
     echo $entry
-    cargo run -- ../$search_dir$entry || { echo 'mg test failed' ; exit 1; }
+    cargo llvm-cov --no-report run ../$search_dir$entry || { echo 'mg test failed' ; exit 1; }
 done
+
+cargo llvm-cov --cobertura --ignore-filename-regex "utils/|experimental/" --output-path ../../target/cobertura.xml report
