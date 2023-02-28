d505546774e58cfad654051bba2992c44f6e7bad
bcral <>
2023-02-28 16:06:18 -0500

    Things pass locally!

--- message-generator-tests.sh
+++ message-generator-tests.sh
@@ -1,6 +1,8 @@
 search_dir="../../test/message-generator/test/"
 message_generator_dir="./utils/message-generator/"
 
+cargo llvm-cov clean
+
 cd $message_generator_dir
 
 for entry in `ls $search_dir`; do
