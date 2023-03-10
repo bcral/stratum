29b2f773c4c37808a7b7cf0a1b860b3af5332672
bcral <>
2023-03-10 16:15:38 -0500

    Why no mining-proxy coverage?

--- message-generator-tests.sh
+++ message-generator-tests.sh
@@ -3,6 +3,8 @@ message_generator_dir="./utils/message-generator/"
 
 cd $message_generator_dir
 
+cargo llvm-cov clean --workspace
+
 for entry in `ls $search_dir`; do
     echo $entry
     cargo run -- ../$search_dir$entry || { echo 'mg test failed' ; exit 1; }
