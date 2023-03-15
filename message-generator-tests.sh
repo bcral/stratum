9be901518ada8056aebba184c3edc7288db0f2a7
bcral <>
2023-03-15 10:40:58 -0400

    Added granularity to llvm ignore utils

--- message-generator-tests.sh
+++ message-generator-tests.sh
@@ -3,7 +3,13 @@ message_generator_dir="./utils/message-generator/"
 
 cd $message_generator_dir
 
+cargo llvm-cov clean
+
 for entry in `ls $search_dir`; do
     echo $entry
     cargo run -- ../$search_dir$entry || { echo 'mg test failed' ; exit 1; }
 done
+
+cd ../../
+
+cargo llvm-cov --ignore-filename-regex "utils/message-generator|experimental/|protocols/" --cobertura --output-path "target/mg_coverage_report.xml" report
